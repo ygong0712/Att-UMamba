4d92f0066d73dae07fd3e547de61d408cf3ed7c2
Yijie Gong <91856596+ygong0712@users.noreply.github.com>
2024-05-10 14:18:38 +0100

    Update hyperparameter
    1000->100
    250->50
    50->25

--- umamba/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ umamba/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -142,9 +142,9 @@ class nnUNetTrainer(object):
         self.initial_lr = 1e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
-        self.num_iterations_per_epoch = 250
-        self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 1000
+        self.num_iterations_per_epoch = 50
+        self.num_val_iterations_per_epoch = 25
+        self.num_epochs = 100
         self.current_epoch = 0
         self.enable_deep_supervision = True
 
